e3d834ca97afcc55ab7a0e0981476746cacaa23d
Kaushik Srenevasan <kaushik@twitter.com>
2014-09-05 16:34:57 -0700

    Initial import.

---
+++ src/test/java/com/twitter/diagnostics/PerfTracepointTest.java
@@ -0,0 +1,38 @@
+package com.twitter.diagnostics;
+
+import java.lang.reflect.Method;
+import java.lang.NoSuchMethodException;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+class Tracepoints {
+    @PerfTracepoint static native void tracepoint0();
+};
+
+public class PerfTracepointTest extends TestCase {
+    public PerfTracepointTest(String name)
+    {
+	super(name);
+    }
+
+    public static Test suite()
+    {
+	return new TestSuite(PerfTracepointTest.class);
+    }
+
+    public void testApp()
+    {
+	Method method = null;
+	try {
+	    method = Tracepoints.class.getDeclaredMethod("tracepoint0");
+	    assertNotNull("Method Tracepoints.tracepoint0 is null.", method);
+	} catch (NoSuchMethodException e) {
+	    assertTrue(false);
+	}
+
+	PerfTracepoint tracepoint = method.getAnnotation(PerfTracepoint.class);
+	assertNotNull("PerfTracepoint instance is null.", tracepoint);
+    }
+};
